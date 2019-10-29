a9cff35dbaaca73256e2f9318f07855ecd211412
SomeoneAtLeast <winecooler@yandex.ru>
2019-10-29 16:02:35 +0300

    Текстовым полям добавлен текст + увеличен размер блока с полями и выполнено базовое позиционирование

--- src/blocks/form-input-subscribe/form-input.js
+++ src/blocks/form-input-subscribe/form-input.js
@@ -1,30 +1,30 @@
-let subscribe__form =  document.querySelector(".subscribe__form");
-let subscribe__email_input =  document.querySelector(".form-input");
-let email_input =  document.querySelector(".form-elements-group-1__email .form-input");
-let pretty_awesome_input =  document.querySelector(".form-elements-group-1__pretty_awesome_text .form-input");
+if(window.location.toString().indexOf('headers_and_footers.html')>0) {
 
-subscribe__form.addEventListener("submit", function (evt) {
-	if (!subscribe__email_input.value) {
-			evt.preventDefault();
-			subscribe__email_input.classList.add("form-input--wrong");
-			subscribe__email_input.addEventListener("click", function (evt) {
-			subscribe__email_input.classList.remove("form-input--wrong"); 
-			}
-		)    	
-	}
-})
+	let subscribe__form =  document.querySelector(".subscribe__form");
+	let subscribe__email_input =  document.querySelector(".form-input");
+	let email_input =  document.querySelector(".form-elements-group-1__email .form-input");
+	let pretty_awesome_input =  document.querySelector(".form-elements-group-1__pretty_awesome_text .form-input");
 
-subscribe__email_input.onblur = function() {
-if (subscribe__email_input.value) {
- 	subscribe__email_input.classList.add("form-input--active");
- 	} else {
- 		 subscribe__email_input.classList.remove("form-input--active");
- 	}
-} 
+	subscribe__form.addEventListener("submit", function (evt) {
+		if (!subscribe__email_input.value) {
+				evt.preventDefault();
+				subscribe__email_input.classList.add("form-input--wrong");
+				subscribe__email_input.addEventListener("click", function (evt) {
+				subscribe__email_input.classList.remove("form-input--wrong"); 
+				}
+			)    	
+		}
+	})
 
-console.log("2")
-alert("2")
+	subscribe__email_input.onblur = function() {
+	if (subscribe__email_input.value) {
+ 		subscribe__email_input.classList.add("form-input--active");
+ 		} else {
+ 		 	subscribe__email_input.classList.remove("form-input--active");
+ 		}
+	} 
+}
 
-document.querySelector(".form-elements-group-1__email .form-input").placeholder = 'новый текст';
+let pretty_awesome_input = document.querySelector(".form-elements-group-1__pretty_awesome_text .form-input");
 
-pretty_awesome_input.placeholder = 'новый текст';
+pretty_awesome_input.placeholder = "This is pretty awesome";
--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,67 +1,70 @@
-let menu_open_btn = document.querySelector(".main-nav__btn");
-let main_nav__list = document.querySelector(".main-nav__list");
+if(window.location.toString().indexOf('headers_and_footers.html')>0) {
 
-if (document.querySelector(".js-off") !== null && document.querySelector(".js-off").classList.contains("js-off")) {
-	console.log("Страница с шаблонами элементов, JS в хедере выключен из-за повторов блока");
-} else {
+	let menu_open_btn = document.querySelector(".main-nav__btn");
+	let main_nav__list = document.querySelector(".main-nav__list");
 
-	main_nav__list.classList.remove("nojs");
+	if (document.querySelector(".js-off") !== null && document.querySelector(".js-off").classList.contains("js-off")) {
+		console.log("Страница с шаблонами элементов, JS в хедере выключен из-за повторов блока");
+	} else {
 
-	if (typeof document.querySelector(".login-register") !== "undefined" &&
- 	document.querySelector(".login-register") !== null) {
+		main_nav__list.classList.remove("nojs");
 
-		let login_register = document.querySelector(".login-register");
+		if (typeof document.querySelector(".login-register") !== "undefined" &&
+ 		document.querySelector(".login-register") !== null) {
 
-		login_register.classList.remove("login-register--nojs");
+			let login_register = document.querySelector(".login-register");
 
-		menu_open_btn.addEventListener("click", function (evt) {
+			login_register.classList.remove("login-register--nojs");
+
+			menu_open_btn.addEventListener("click", function (evt) {
+				if (!menu_open_btn.classList.contains("main-nav__btn--open-menu")) {
+					evt.preventDefault();
+					menu_open_btn.classList.add("main-nav__btn--open-menu");
+					main_nav__list.classList.add("open-menu");
+					login_register.classList.add("open-reg");
+				} else if (menu_open_btn.classList.contains("main-nav__btn--open-menu")
+		 			) {
+					menu_open_btn.classList.remove("main-nav__btn--open-menu");
+					main_nav__list.classList.remove("open-menu")
+					login_register.classList.remove("open-reg");
+					login_register.classList.add("close-reg");
+
+					main_nav__list.classList.add("close-menu");
+
+					setTimeout(function(){
+					main_nav__list.classList.remove("close-menu");
+					login_register.classList.remove("close-reg");
+						}, 300);
+				}
+			})
+		} else if (typeof document.querySelector(".successful-login") !== "undefined" &&
+ 		document.querySelector(".successful-login") !== null) {
+
+			let successful_login = document.querySelector(".successful-login");
+
+			successful_login.classList.remove("successful-login--nojs");
+
+			menu_open_btn.addEventListener("click", function (evt) {
 			if (!menu_open_btn.classList.contains("main-nav__btn--open-menu")) {
 				evt.preventDefault();
 				menu_open_btn.classList.add("main-nav__btn--open-menu");
 				main_nav__list.classList.add("open-menu");
-				login_register.classList.add("open-reg");
+				successful_login.classList.add("open-succes-log");
 			} else if (menu_open_btn.classList.contains("main-nav__btn--open-menu")
 		 		) {
 				menu_open_btn.classList.remove("main-nav__btn--open-menu");
 				main_nav__list.classList.remove("open-menu")
-				login_register.classList.remove("open-reg");
-				login_register.classList.add("close-reg");
+				successful_login.classList.remove("open-succes-log");
+				successful_login.classList.add("close-succes-log");
 
 				main_nav__list.classList.add("close-menu");
 
 				setTimeout(function(){
 				main_nav__list.classList.remove("close-menu");
-				login_register.classList.remove("close-reg");
+				successful_login.classList.remove("close-succes-log");
 					}, 300);
-			}
-		})
-	} else if (typeof document.querySelector(".successful-login") !== "undefined" &&
- 	document.querySelector(".successful-login") !== null) {
-
-		let successful_login = document.querySelector(".successful-login");
-
-		successful_login.classList.remove("successful-login--nojs");
-
-		menu_open_btn.addEventListener("click", function (evt) {
-		if (!menu_open_btn.classList.contains("main-nav__btn--open-menu")) {
-			evt.preventDefault();
-			menu_open_btn.classList.add("main-nav__btn--open-menu");
-			main_nav__list.classList.add("open-menu");
-			successful_login.classList.add("open-succes-log");
-		} else if (menu_open_btn.classList.contains("main-nav__btn--open-menu")
-		 	) {
-			menu_open_btn.classList.remove("main-nav__btn--open-menu");
-			main_nav__list.classList.remove("open-menu")
-			successful_login.classList.remove("open-succes-log");
-			successful_login.classList.add("close-succes-log");
-
-			main_nav__list.classList.add("close-menu");
-
-			setTimeout(function(){
-			main_nav__list.classList.remove("close-menu");
-			successful_login.classList.remove("close-succes-log");
-				}, 300);
-			}
-		})
+				}
+			})
+		}
 	}
 }
