4ecdd6bb3476ef4ba5f5bfd976ce3af19b3db185
Вадим <Vadim@iMac-Apple.local>
2020-02-19 20:08:09 +0300

    Добавление фильтра по датам и полея Подписаться

---
+++ src/blocks/form-elements-group-1/form-elements-group-1.js
@@ -0,0 +1,7 @@
+let pretty_awesome_input = document.querySelector(".form-elements-group-1__pretty_awesome_text .form-input");
+let date_input = document.querySelector(".form-elements-group-1__date-input .form-input");
+let subscribe_input = document.querySelector(".form-elements-group-1__subscribe .form-input");
+
+pretty_awesome_input.placeholder = "This is pretty awesome";
+date_input.placeholder = "ДД.ММ.ГГГГ";
+subscribe_input.placeholder = "Email";
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './blocks/header/header.js'
 import './blocks/form-input-subscribe/form-input.js'
+import './blocks/form-elements-group-1/form-elements-group-1.js'
 import './assets/scss/main.scss'
