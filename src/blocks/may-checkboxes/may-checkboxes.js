22a7096c506041d516a59fb3df6c37005903a3b4
Вадим <Vadim@iMac-Apple.local>
2020-02-20 00:02:57 +0300

    Добавлен блок чекбоксов

---
+++ src/blocks/may-checkboxes/may-checkboxes.js
@@ -0,0 +1,6 @@
+let checkbox_1 = document.querySelector(".may-checkboxes__checkbox-1 .checkbox__input");
+let checkbox_2 = document.querySelector(".may-checkboxes__checkbox-2 .checkbox__input");
+let checkbox_3 = document.querySelector(".may-checkboxes__checkbox-3 .checkbox__input");
+
+checkbox_2.checked=true;
+checkbox_3.checked=true;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './blocks/header/header.js'
 import './blocks/form-input-subscribe/form-input.js'
 import './blocks/form-elements-group-1/form-elements-group-1.js'
+import './blocks/may-checkboxes/may-checkboxes.js'
 import './assets/scss/main.scss'
